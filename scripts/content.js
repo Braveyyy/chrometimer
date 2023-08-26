9e147553f36c0bb179391e326732277a347c6edd
Adam <latifasedyeame@gmail.com>
2023-08-25 19:17:58 -0600

    Started work on javascript portion

---
+++ scripts/content.js
@@ -0,0 +1,13 @@
+var time = document.getElementById("timeNum").value;    
+document.getElementById("form").onsubmit = function() {chromeTimer()};
+
+function chromeTimer()
+{
+    // Change CSS to show timer (Already done when form is submitted.)
+    
+    // Start timer
+    
+    // End timer
+
+    // Send user a notification
+}
