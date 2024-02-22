7b388bfa439b8307cc91b70de5e8bf75f2851f0f
Adam <latifasedyeame@gmail.com>
2024-02-21 21:54:56 -0700

    update

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,45 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.cmd === 'START_TIMER') {
-      timerTime = new Date(request.when);
-      timerID = setTimeout(() => {
-         // the time is app, alert the user.
-      }, timerTime.getTime() - Date.now());
-    } else if (request.cmd === 'GET_TIME') {
-      sendResponse({ time: timerTime });
+document.addEventListener("DOMContentLoaded", function() {
+document.getElementById("powerBtn").addEventListener("click", chromeTimer);
+  var timerDisplay = document.querySelector('#timeCountdown');
+  var intervalID;
+  // Start timer
+  function chromeTimer()
+  {
+    //document.getElementById("tField").style.display = "none";
+    //document.getElementById("tTracker").style.display = "block";
+    var studyTime = 60 * 25; // Hard coded variable for amount of time studied.
+    var timerTime = studyTime, minutes, seconds;
+  
+    intervalID = setInterval(function () {
+        minutes = parseInt(timerTime / 60, 10);
+        seconds = parseInt(timerTime % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        timerDisplay.innerHTML = minutes + ":" + seconds;
+
+        if(--timerTime < 0)
+        {
+            timerTime = studyTime;
+        }
+        chrome.runtime.sendMessage({action: 'updateTimer', value: timerDisplay});
+    }, 1000); 
+  
+      // Send user a notification
+  }
+  // End timer
+  function endTimer()
+  {
+    clearInterval(intervalID);
+  }
+  chrome.runtime.onMessage.addListener((message, sender, response) => {
+    if(message.action === 'startTimer')
+    {
+      chromeTimer();
     }
-  });
+    else if(message.action === 'stopTimer')
+    {
+      endTimer();
+    }
+  })
+});
--- scripts/content.js
+++ scripts/content.js
@@ -1,39 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-chrome.runtime.sendMessage({ cmd: 'GET_TIME' }, response => {
-    if (response.time) {
-        const time = new Date(response.time);
-        chromeTimer(time)
-    }
-    });
-document.getElementById("powerBtn").addEventListener("click", chromeTimer);
-var timerDisplay = document.querySelector('#timeCountdown');
-var intervalID;
-function chromeTimer()
+updateTimerDisplay(value)
 {
-    //document.getElementById("tField").style.display = "none";
-    //document.getElementById("tTracker").style.display = "block";
-    var studyTime = 60 * 25; // Hard coded variable for amount of time studied.
-    var timerTime = studyTime, minutes, seconds;
-    clearInterval(intervalID);
-    intervalID = setInterval(function () {
-        minutes = parseInt(timerTime / 60, 10);
-        seconds = parseInt(timerTime % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        timerDisplay.innerHTML = minutes + ":" + seconds;
-
-        if(--timerTime < 0)
-        {
-            timerTime = studyTime;
-        }
-        
-    }, 1000);
-    // Start timer
     
-    // End timer
-
-    // Send user a notification
 }
 
-});
+chrome.runtime.onMessage.addListener((message, sender, response) => {
+    if(message.action === 'updateTimer')
+    {
+        updateTimerDisplay(message.value);
+    }
+})
